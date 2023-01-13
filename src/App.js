e6b0eca1dd328b59c21538b4e64ca1a81c314bc1
Olvera Deanda <colveradeanda@deloitte.com>
2023-01-13 15:42:24 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Contact } from './model/contact';
+import ComponentA from './components/componentA';
 
 function App() {
+
+  const contactTest = new Contact('Cristobal', 'Olvera', 'colvera@gmail.com', true);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <ComponentA contact={contactTest} />
+        
+        
       </header>
     </div>
   );
